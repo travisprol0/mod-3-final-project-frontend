cbd8794dd7ea88471a54ff92f9ec90c8aed79db1
travisprol0 <travisjohnprol@gmail.com>
2020-07-28 15:32:00 -0400

    arrows

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const URL = 'http://localhost:3000/'
 const [itemURL, charURL, advItmURL, recURL, roomURL, invURL] = [URL+'items', URL+'characters', URL+'advanced_items', URL+'recipes', URL+'rooms', URL+'inventories'];
+let roomNumber = 0
 
 const getItems = () => {
     fetch(itemURL)
     .then(response => response.json())
     .then(items => {
-        console.log(items)
     })
 }
 
@@ -13,22 +13,47 @@ const getRooms = () => {
     fetch(roomURL)
     .then(response => response.json())
     .then(rooms => {
-        console.log(rooms)
-        renderRoom(rooms[0])
+        renderRoom(rooms[roomNumber])
+        const arrows = document.querySelectorAll(".arrow")
+        arrows.forEach(arrow => arrowEventLister(arrow, rooms))
+    })
+}
+
+const arrowEventLister = (arrow, rooms) => {
+    arrow.addEventListener("mouseover", (e) => {
+        e.target.style.background = 'rgba(0,0,0,0.4)';
+    }),
+    arrow.addEventListener("mouseout", (e) => {
+        e.target.style.background = 'rgba(0,0,0,0)';
+    }),
+    arrow.addEventListener("click", (e) => {
+        if (e.target.className === "arrow right"){
+            if (roomNumber < 3) {
+                roomNumber ++
+                renderRoom(rooms[roomNumber])
+            }
+        }
+        else if (e.target.className === "arrow left"){
+            if (roomNumber > 0) {
+                roomNumber --
+                renderRoom(rooms[roomNumber])
+            }
+        }
     })
 }
 
 const renderRoom = room => {
     const div = document.querySelector('.game-div')
-    console.log(room.img_url)
+    div.id = room.name
     div.style.backgroundImage = `url('${room.img_url}')`
+
+
 }
 
 const getAdvItems = () => {
     fetch(advItmURL)
     .then(response => response.json())
     .then(advItms => {
-        console.log(advItms)
     })
 }
 
@@ -36,7 +61,6 @@ const getChar = () => {
     fetch(charURL)
     .then(response => response.json())
     .then(char => {
-        console.log(char)
     })
 }
 
@@ -44,7 +68,6 @@ const getRecs = () => {
     fetch(recURL)
     .then(response => response.json())
     .then(recipes => {
-        console.log(recipes)
     })
 }
 
@@ -52,7 +75,6 @@ const getInvetory = () => {
     fetch(invURL)
     .then(response => response.json())
     .then(inventory => {
-        console.log(inventory)
     })
 }
 
