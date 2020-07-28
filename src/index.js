169bf55deddfdbbf1d150025c26b1c8326092cff
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-28 13:26:41 -0400

    loaded image background

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const URL = 'http://localhost:3000/'
-const [itemURL, charURL, advItmURL, recURL] = [URL+'items', URL+'characters', URL+'advanced_items', URL+'recipes'];
+const [itemURL, charURL, advItmURL, recURL, roomURL, invURL] = [URL+'items', URL+'characters', URL+'advanced_items', URL+'recipes', URL+'rooms', URL+'inventories'];
 
 const getItems = () => {
     fetch(itemURL)
@@ -9,8 +9,60 @@ const getItems = () => {
     })
 }
 
+const getRooms = () => {
+    fetch(roomURL)
+    .then(response => response.json())
+    .then(rooms => {
+        console.log(rooms)
+        renderRoom(rooms[0])
+    })
+}
+
+const renderRoom = room => {
+    const div = document.querySelector('.game-div')
+    console.log(room.img_url)
+    div.style.backgroundImage = `url('${room.img_url}')`
+}
+
+const getAdvItems = () => {
+    fetch(advItmURL)
+    .then(response => response.json())
+    .then(advItms => {
+        console.log(advItms)
+    })
+}
+
+const getChar = () => {
+    fetch(charURL)
+    .then(response => response.json())
+    .then(char => {
+        console.log(char)
+    })
+}
+
+const getRecs = () => {
+    fetch(recURL)
+    .then(response => response.json())
+    .then(recipes => {
+        console.log(recipes)
+    })
+}
+
+const getInvetory = () => {
+    fetch(invURL)
+    .then(response => response.json())
+    .then(inventory => {
+        console.log(inventory)
+    })
+}
+
 const main = () => {
+    getRooms()
     getItems()
+    getAdvItems()
+    getRecs()
+    getChar()
+    getInvetory()
 }
 
 
